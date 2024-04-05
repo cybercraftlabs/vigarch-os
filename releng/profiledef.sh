1b450211ab2ad5971bc801588aea4ce9ea09caf3
Gaurav Raj <techw803@gmail.com>
2024-04-05 18:06:01 +0530

    Customized the syslinux bootloader

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="vigilante"
+iso_name="vigarch"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_publisher="Vigilante Arch <https://vigarch.thehackersbrain.online>"
+iso_application="Vigilante Arch Live/Rescue/Hacking System"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
